46ff03ac37e67941a4e19aa24b22890c3f5c2152
Rachel <paintmetothemoon@gmail.com>
2017-11-06 13:15:42 -0800

    Updating comments

--- oldMcdonald.py
+++ oldMcdonald.py
@@ -1,20 +1,18 @@
 #oldMacdonald.py
-#Write a program to print the lyrics of the song "Old MacDonald."
-#The program should print the lyrics for five different animals.
-#put parameter 'noise' in a tuple!
-
-     
+# this is a prigram to print out lyrics using functions and the modulus opporator as a variable. 
  
+#creating a funtion for the lyric sequence     
 def sing(animal,noise):
     sound = tuple([noise]*8)
-    chorus = "Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!"
+    chorus = "Old MacDonald had a farm, Ee-i, Ee-i, Oh!"
     print chorus
-    print "And on that farm he had a %s, Ee-igh, Ee-igh, Oh!" %animal
+    print "And on that farm he had a %s, Ee-i, Ee-i, Oh!" %animal
     print "With a %s, %s here and a %s, %s there.\n\
 Here a %s, there a %s, everywhere a %s, %s." %sound
     print chorus
  
-         
+#Creating another function for our chosen animals and noises
+#This function calls from the first function     
 def main():
     sing("cow", "moo")
     sing("horse", "neigh")
@@ -23,8 +21,3 @@ def main():
     sing("chick", "cheep")
      
 main()
-
-#m,f="Old MacDonald had a farm, E-I-E-I-O",
-#lambda x,y:m+",\nAnd on that farm he had a %s, E-I-E-I-O,\nWith a %shere and a %sthere,\nHere a %s, there a %s, everywhere a %s %s,\n%s!"
-#%((x,)+((y+' ')*2,)*2+(y,)*4+(m,))
-
